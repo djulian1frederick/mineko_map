91b263f146e4b12cb0ec9c73bcb4fb4a501ee807
ArsenalNox <nohtimore@gmail.com>
2021-12-28 10:42:58 +0500

    Подсписок городов в списке муниципалитетов

--- index.php
+++ index.php
@@ -17,10 +17,26 @@
 		<ul>
 			<?php 
 				require_once('admin/connection.php');
-				$result_raions = mysqli_query($bd, "SELECT raion, id_raion from raions");
-				while($row_raions=mysqli_fetch_array($result_raions)){
+				$result_raions = mysqli_query($bd, "SELECT raion, id_raion from raions WHERE city_mo_id IS NULL");
+                while($row_raions=mysqli_fetch_array($result_raions)){
+
+                    $isCityListEmpty = true;
+                    $sublist_cities = '<li><ul class="inner-city-list">';
+                    $result_cities = mysqli_query($bd, "SELECT raion, id_raion FROM raions WHERE city_mo_id = '".$row_raions['id_raion']."';");
+                    while($row_cities=mysqli_fetch_array($result_cities)){
+                        $isCityListEmpty = false;
+                        $id_c = "r".$row_cities['id_raion'];
+                        $sublist_cities = $sublist_cities.'
+                            <li onmousemove="hovermap(`'.$id_c.'`)">
+                                <a href="page_mo.php?id='.$row_cities['id_raion'].'">'.$row_cities['raion'].'</a>
+                            </li>';
+                    }
+                    $sublist_cities = $sublist_cities.'</ul></li>';
+                    if($isCityListEmpty) $sublist_cities = '';
+
+                    
 				 	$id_r = "r".$row_raions['id_raion'];
-				 	echo '<li onmousemove="hovermap(`'.$id_r.'`)"><a href="page_mo.php?id='.$row_raions['id_raion'].'">'.$row_raions['raion'].'</a></li>';
+				 	echo '<li onmousemove="hovermap(`'.$id_r.'`)"><a href="page_mo.php?id='.$row_raions['id_raion'].'">'.$row_raions['raion'].'</a></li>'.$sublist_cities.'';
 				}
 
 
