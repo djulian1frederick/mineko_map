c21312492a6cdafef9ddde13cbc78437db7207e1
djulian1frederick <juliaplotnikov@yandex.ru>
2021-12-28 03:01:42 -0500

    28.12 true

--- nah.php
+++ nah.php
@@ -1,12 +1,34 @@
-<ul class="nice_navigation">
+<ul class="map-navigation">
 	<a href=""><li>Главная</li></a>
 	<a href=""><li>Анализ</li></a>
 	<a href=""><li>Рейтинг</li></a>
 	<a href=""><li>Достижения</li></a>
 	<a href=""><li>Общее</li></a>
-	<li style="display: inline-flex; width: 270px;"><input type="text" name="search"><button type="submit"><img src="img/search.png"></button></li>
 </ul>
 
 <style>
+
+	body { background: #000; }
+	.map-navigation {
+		width: 300px;
+		list-style: none;
+		padding: 0;
+		margin:  0;
+	}
+	.map-navigation li {
+		font-family: Circe ExtraLight;
+		padding: 5px 7px;
+		margin: 1px 0;
+		background: #fff;
+		color:  black;
+	}
+	.map-navigation a {
+		text-decoration: none;
+	}
+
+	.map-navigation li:hover {
+		background: #1c75bc;
+		color: white;
+	}
 	
 </style>
