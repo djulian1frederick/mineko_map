1564d2bd68d3d3e34165c9a58e17a6b874bfd57a
djulian1frederick <juliaplotnikov@yandex.ru>
2022-02-28 10:58:52 -0500

    actual

--- admin/personal_cabinet/cancel_reg.php
+++ admin/personal_cabinet/cancel_reg.php
@@ -1,14 +1,14 @@
 <?php 	session_start(); ?>
 <?php require_once('../connection.php'); ?>
 <div class="blocks_info">
-	<form action="/editor/scripts/delete_account.php" method="post" target="information">
+	<form action="/editor/scripts/delete_account.php" method="post">
 	<?php echo '<input type="hidden" name="userid" value="'.$_SESSION['user_id'].'">';?>
 	<button onclick="confirm_cancel()" value="personal" name="button" type="submit">Я хочу удалить свою учетную запись</button>
 <?php if($_SESSION['level'] == '0') { echo'<button onclick="confirm_cancel()" value="zayavka" name="button" type="submit">Я хочу удалить заявку на регистрацию организации в системе и свою учетную запись</button>';}?>
 
 	<p>Будьте аккуратны, данные действия нельзя отменить.</p>
 	</form>
-	<iframe borderless style="height: 40px; width: 100%;" name="information"></iframe>
+
 </div>
 
 
