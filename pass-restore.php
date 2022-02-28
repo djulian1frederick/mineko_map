0188ef1a3fb8bbb7fdd9930972827a199d00e741
djulian1frederick <juliaplotnikov@yandex.ru>
2022-02-28 07:10:09 -0500

    actual

--- pass-restore.php
+++ pass-restore.php
@@ -4,11 +4,11 @@
 	</div>
 	<div class="popup" style="height: 250px;">
 		<div style="height: 200px;">
-			<p style="color: #7b6f6f;">Для того, чтобы сбросить пароль, введите в поле ниже e-mail, который был использован для регистрации</p>
+			<p style="color: #7b6f6f; font-size: 14px; font-family: Circe ExtraLight;">Для того, чтобы сбросить пароль, введите в поле ниже e-mail, который был использован для регистрации</p>
 			<br><input type="email" id="email" required placeholder="введите адрес электронной почты">
-			<div id="result" style="margin-top: 15px;"></div>
+			<div id="result" style="margin-top: 10px;"></div>
 		</div>
-		<div style="display: flex; flex-flow: row;">
+		<div style="display: flex; flex-flow: row; margin-top: 10px;">
 			<div class="popup-yes" onclick="restore_password()">
 				<p>сбросить</p>
 			</div>
