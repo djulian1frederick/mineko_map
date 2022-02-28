eadc457a5cb18da917df04e2639647b167aa1ae6
djulian1frederick <juliaplotnikov@yandex.ru>
2022-02-28 09:16:27 -0500

    actual

--- pass-restore.php
+++ pass-restore.php
@@ -4,8 +4,8 @@
 	</div>
 	<div class="popup" style="height: 250px;">
 		<div style="height: 200px;">
-			<h6 class="exit" onclick="abort_operation()">X</h6>
-			<p style="color: #f43038; font-size: 14px; font-family: Circe Light;">Для того, чтобы сбросить пароль, введите в поле ниже e-mail, который был использован для регистрации</p>
+			<h6 class="exit" onclick="abort_operation()">x</h6>
+			<p style="color: #f43038; font-size: 16px; font-family: Circe Light;">Для того, чтобы сбросить пароль, введите в поле ниже e-mail, который был использован для регистрации</p>
 			<br><input type="email" id="email" required placeholder="введите адрес электронной почты" style="width: 90%;">
 			<div id="result" style="margin-top: 10px;"></div>
 		</div>
